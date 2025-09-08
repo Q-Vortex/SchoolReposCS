714b64fa99c1a058d8491b34c9927573a43f0260
Q-Vortex <sceletonboy223@gmail.com>
2025-09-08 11:07:40 +0300

    Leason2 Init

---
+++ leason2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.ExceptionServices;  // Namespace for basic system functions
+
+// Namespace declaration
+namespace MyApplication
+{
+    // Class declaration
+    class Program
+    {
+        // Main method — entry point of the program
+        static void Main(string[] args)
+        {
+            Stack<int> stiva = new Stack<int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                stiva.Push(n % 2);
+                n /= 2;
+            }
+
+            foreach (int num in stiva)
+            {
+                Console.Write(num);
+            }
+        }
+    }
+}
