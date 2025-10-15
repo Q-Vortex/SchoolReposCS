f2ae7b9a3f36e5003d234e5ebb92a9a64b3a30b6
Q-Vortex <sceletonboy223@gmail.com>
2025-10-15 09:28:46 +0300

    (Update) DIR NAME

---
+++ workout/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace workout;
+
+class Program
+{
+
+    class Node
+    {
+        public int value;
+        public int left;
+        public int right;
+    }
+
+    static void Main(string[] args)
+    {
+        Node node = new Node();
+    }
+}
